eeebbf2ae094175e58df834f347aa173106917d2
MarkRidings <Mark.Ridings@greenwayhealth.com>
2018-03-07 15:35:26 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,90 @@
+const ZwiftAccount = require('zwift-mobile-api');
+const Result = require('./results');
+const account = new ZwiftAccount("ridings.mark@gmail.com", "Acesfull1");
+const event = account.getEvent();
+
+function msToTime(duration) {
+    var milliseconds = parseInt((duration%1000)/100)
+        , seconds = parseInt((duration/1000)%60)
+        , minutes = parseInt((duration/(1000*60))%60)
+        , hours = parseInt((duration/(1000*60*60))%24);
+
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+}
+/*
+function getSubResults(subResultId) {
+    return event.segmentResults(subResultId);
+}
+*/
+
+const options = {
+    eventStartsAfter: new Date(2018, 2, 7, 5, 33, 0).getTime(),
+    eventStartsBefore: new Date(2018, 2, 7, 5, 38, 0).getTime()
+};
+
+event.search(options).then(races => {
+    const subGroups = races[0].eventSubgroups;
+    const results = new Result();
+
+    results.getResults(subGroups).then(raceResults => {
+        //console.log(raceResults);
+        let j = 0;
+        for (let i = 0; i < raceResults.length; i++) {
+            console.log(`${j}. ${raceResults[i].firstName} ${raceResults[i].lastName} - ${msToTime(raceResults[i].elapsedMs)} | ${(raceResults[i].power / (raceResults[i].weight / 1000)).toFixed(2)}`);
+            j++;
+        }
+    }).catch(function(err) {
+        console.log("ERROR: ", err);
+    });
+    
+});
+
+/*
+let segResults = [];
+let raceResults = [];
+
+event.search(options).then (results => {
+    const subGroups = results[0].eventSubgroups;    
+    for (let i = 0; i < subGroups.length; i++) {
+        segResults.push(getSubResults(subGroups[i].id));
+    }
+}).then(() => {
+    Promise.all(segResults).then(subGroupResults => {
+        for (let i = 0; i < subGroupResults.length; i++) {
+            for (let j = 0; j < subGroupResults[i].length; j++) {
+                raceResults.push(subGroupResults[i][j]);
+            }
+        }
+
+        raceResults.sort((a, b) => {
+            if (a.elapsedMs > b.elapsedMs) {
+                return 1;
+            }
+            else if (a.elapsedMs === b.elapsedMs) {
+                return 0;
+            }
+            return -1;
+        });
+
+        let j = 1;
+        for (let i = 0; i < raceResults.length; i++) {
+            //if (i === 0) { console.log(raceResults[i]); }
+            if (raceResults[i].powermeter) {
+                console.log(`${j}. ${raceResults[i].firstName} ${raceResults[i].lastName} - ${msToTime(raceResults[i].elapsedMs)} | ${(raceResults[i].power / (raceResults[i].weight / 1000)).toFixed(2)}`);
+                j++;
+            }
+        }
+    }).catch(function (err) {
+        console.log("ERROR: ", err) ;
+    });
+}).catch(function (err) {
+    console.log("ERROR: ", err);
+});
+*/
+
+
+
---
+++ results.js
@@ -0,0 +1,42 @@
+const ZwiftAccount = require('zwift-mobile-api');
+const account = new ZwiftAccount("ridings.mark@gmail.com", "Acesfull1");
+const event = account.getEvent();
+
+function getSubResults(subResultId) {
+    return event.segmentResults(subResultId);
+}
+
+class Result {
+    
+    getResults(subGroups) {
+        let segResults = [];
+        let raceResults = [];
+
+        for (let i = 0; i < subGroups.length; i++) {
+            segResults.push(getSubResults(subGroups[i].id));
+        }
+
+        return Promise.all(segResults).then(subGroupResults => {
+            for (let i = 0; i < subGroupResults.length; i++) {
+                for (let j = 0; j < subGroupResults[i].length; j++) {
+                    if (subGroupResults[i][j].powermeter) {
+                        raceResults.push(subGroupResults[i][j]);
+                    }
+                }
+            }
+            raceResults.sort((a, b) => {
+                if (a.elapsedMs > b.elapsedMs) {
+                    return 1;
+                }
+                else if (a.elapsedMs === b.elapsedMs) {
+                    return 0;
+                }
+                return -1;
+            });
+
+            return raceResults;
+        });
+    }
+}
+
+module.exports = Result;
